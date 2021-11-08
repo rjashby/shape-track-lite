22a30b71582bef407a168df2f9b820bed8aa2cf9
Ryan Ashby <rjapsu160@gmail.com>
2021-11-08 13:33:36 -0800

    finish working through shape tracker again as per lesson.
    Co-Authored-by: Skylar Brockbank <skylar.brockbank@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import Triangle from './triangle.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+let myTriangle = new Triangle(3, 3, 3);
+
+alert(myTriangle.c);
--- src/triangle.js
+++ src/triangle.js
@@ -1,3 +1,5 @@
 export default function Triangle(side1, side2, side3) {
-  
+  this.a = side1;
+  this.b = side2;
+  this.c = side3;
 }
